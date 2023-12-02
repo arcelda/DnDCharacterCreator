50d346ff5c1dd5b4f8c4a91e3663fbb48e7546d1
arcelda <arceldajactta@gmail.com>
2023-12-02 13:15:57 -0500

    Created, the dataset, changes to form 2, database, searchplayer

--- DnDCharacterCreator/SearchPlayer.Designer.cs
+++ DnDCharacterCreator/SearchPlayer.Designer.cs
@@ -28,26 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
+            this.components = new System.ComponentModel.Container();
+            this.dnd_DatabaseDataSet1 = new DnDCharacterCreator.Dnd_DatabaseDataSet1();
+            this.characterTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.characterTableTableAdapter = new DnDCharacterCreator.Dnd_DatabaseDataSet1TableAdapters.CharacterTableTableAdapter();
+            this.tableAdapterManager = new DnDCharacterCreator.Dnd_DatabaseDataSet1TableAdapters.TableAdapterManager();
+            this.characterTableDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.searchToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.searchToolStripTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.FillSearchToolStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dnd_DatabaseDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterTableDataGridView)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dnd_DatabaseDataSet1
+            // 
+            this.dnd_DatabaseDataSet1.DataSetName = "Dnd_DatabaseDataSet1";
+            this.dnd_DatabaseDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // characterTableBindingSource
+            // 
+            this.characterTableBindingSource.DataMember = "CharacterTable";
+            this.characterTableBindingSource.DataSource = this.dnd_DatabaseDataSet1;
+            // 
+            // characterTableTableAdapter
+            // 
+            this.characterTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CharacterTableTableAdapter = this.characterTableTableAdapter;
+            this.tableAdapterManager.UpdateOrder = DnDCharacterCreator.Dnd_DatabaseDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // characterTableDataGridView
+            // 
+            this.characterTableDataGridView.AllowUserToAddRows = false;
+            this.characterTableDataGridView.AllowUserToDeleteRows = false;
+            this.characterTableDataGridView.AutoGenerateColumns = false;
+            this.characterTableDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.characterTableDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8,
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewTextBoxColumn12,
+            this.dataGridViewTextBoxColumn13});
+            this.characterTableDataGridView.DataSource = this.characterTableBindingSource;
+            this.characterTableDataGridView.Location = new System.Drawing.Point(44, 64);
+            this.characterTableDataGridView.Name = "characterTableDataGridView";
+            this.characterTableDataGridView.ReadOnly = true;
+            this.characterTableDataGridView.Size = new System.Drawing.Size(673, 357);
+            this.characterTableDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "character_ID";
+            this.dataGridViewTextBoxColumn1.HeaderText = "character_ID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "password";
+            this.dataGridViewTextBoxColumn2.HeaderText = "password";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            this.dataGridViewTextBoxColumn2.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "name";
+            this.dataGridViewTextBoxColumn3.HeaderText = "name";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "level";
+            this.dataGridViewTextBoxColumn4.HeaderText = "level";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "gender";
+            this.dataGridViewTextBoxColumn5.HeaderText = "gender";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "race";
+            this.dataGridViewTextBoxColumn6.HeaderText = "race";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "role";
+            this.dataGridViewTextBoxColumn7.HeaderText = "role";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "strength";
+            this.dataGridViewTextBoxColumn8.HeaderText = "strength";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            this.dataGridViewTextBoxColumn8.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.DataPropertyName = "dexterity";
+            this.dataGridViewTextBoxColumn9.HeaderText = "dexterity";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            this.dataGridViewTextBoxColumn9.ReadOnly = true;
+            this.dataGridViewTextBoxColumn9.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "constitution";
+            this.dataGridViewTextBoxColumn10.HeaderText = "constitution";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            this.dataGridViewTextBoxColumn10.ReadOnly = true;
+            this.dataGridViewTextBoxColumn10.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "intelligence";
+            this.dataGridViewTextBoxColumn11.HeaderText = "intelligence";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            this.dataGridViewTextBoxColumn11.ReadOnly = true;
+            this.dataGridViewTextBoxColumn11.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn12
+            // 
+            this.dataGridViewTextBoxColumn12.DataPropertyName = "wisdom";
+            this.dataGridViewTextBoxColumn12.HeaderText = "wisdom";
+            this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            this.dataGridViewTextBoxColumn12.ReadOnly = true;
+            this.dataGridViewTextBoxColumn12.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn13
+            // 
+            this.dataGridViewTextBoxColumn13.DataPropertyName = "charisma";
+            this.dataGridViewTextBoxColumn13.HeaderText = "charisma";
+            this.dataGridViewTextBoxColumn13.Name = "dataGridViewTextBoxColumn13";
+            this.dataGridViewTextBoxColumn13.ReadOnly = true;
+            this.dataGridViewTextBoxColumn13.Visible = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchToolStripLabel,
+            this.searchToolStripTextBox,
+            this.FillSearchToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // searchToolStripLabel
+            // 
+            this.searchToolStripLabel.Name = "searchToolStripLabel";
+            this.searchToolStripLabel.Size = new System.Drawing.Size(159, 22);
+            this.searchToolStripLabel.Text = "Enter the name of the player:";
+            // 
+            // searchToolStripTextBox
+            // 
+            this.searchToolStripTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.searchToolStripTextBox.Name = "searchToolStripTextBox";
+            this.searchToolStripTextBox.Size = new System.Drawing.Size(100, 25);
+            // 
+            // FillSearchToolStripButton
+            // 
+            this.FillSearchToolStripButton.BackColor = System.Drawing.Color.Orange;
+            this.FillSearchToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.FillSearchToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.FillSearchToolStripButton.Name = "FillSearchToolStripButton";
+            this.FillSearchToolStripButton.Size = new System.Drawing.Size(81, 22);
+            this.FillSearchToolStripButton.Text = "Search Player";
             // 
             // SearchPlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.characterTableDataGridView);
             this.Name = "SearchPlayer";
             this.Text = "SearchPlayer";
+            this.Load += new System.EventHandler(this.SearchPlayer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dnd_DatabaseDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.characterTableDataGridView)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
-=======
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SearchPlayer";
->>>>>>> 0cca3bb2912fa0de8f9893c5d0ccacd3a81e035b
         }
 
         #endregion
+
+        private Dnd_DatabaseDataSet1 dnd_DatabaseDataSet1;
+        private System.Windows.Forms.BindingSource characterTableBindingSource;
+        private Dnd_DatabaseDataSet1TableAdapters.CharacterTableTableAdapter characterTableTableAdapter;
+        private Dnd_DatabaseDataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView characterTableDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn13;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel searchToolStripLabel;
+        private System.Windows.Forms.ToolStripTextBox searchToolStripTextBox;
+        private System.Windows.Forms.ToolStripButton FillSearchToolStripButton;
     }
 }
--- DnDCharacterCreator/SearchPlayer.cs
+++ DnDCharacterCreator/SearchPlayer.cs
@@ -16,5 +16,20 @@ namespace DnDCharacterCreator
         {
             InitializeComponent();
         }
+
+        private void characterTableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.characterTableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dnd_DatabaseDataSet1);
+
+        }
+
+        private void SearchPlayer_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dnd_DatabaseDataSet1.CharacterTable' table. You can move, or remove it, as needed.
+            this.characterTableTableAdapter.Fill(this.dnd_DatabaseDataSet1.CharacterTable);
+
+        }
     }
 }
