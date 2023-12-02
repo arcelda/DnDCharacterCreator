f66c0fa5b6d7eb0e5b1620e85f1b2041b5a63999
arcelda <arceldajactta@gmail.com>
2023-12-02 13:22:42 -0500

    Player search Query

--- DnDCharacterCreator/Dnd_DatabaseDataSet1.Designer.cs
+++ DnDCharacterCreator/Dnd_DatabaseDataSet1.Designer.cs
@@ -1273,12 +1273,19 @@ namespace DnDCharacterCreator.Dnd_DatabaseDataSet1TableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT character_ID, password, name, [level], gender, race, role, strength, dexte" +
                 "rity, constitution, intelligence, wisdom, charisma FROM dbo.CharacterTable";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT        character_ID, password, name, level, gender, race, role, strength, " +
+                "dexterity, constitution, intelligence, wisdom, charisma\r\nFROM            Charact" +
+                "erTable\r\nWHERE        (name LIKE \'%\' + @search + \'%\')";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@search", global::System.Data.SqlDbType.NVarChar, 255, global::System.Data.ParameterDirection.Input, 0, 0, "name", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -1303,6 +1310,25 @@ namespace DnDCharacterCreator.Dnd_DatabaseDataSet1TableAdapters {
             Dnd_DatabaseDataSet1.CharacterTableDataTable dataTable = new Dnd_DatabaseDataSet1.CharacterTableDataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBySearchName(Dnd_DatabaseDataSet1.CharacterTableDataTable dataTable, string search) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((search == null)) {
+                this.Adapter.SelectCommand.Parameters[0].Value = global::System.DBNull.Value;
+            }
+            else {
+                this.Adapter.SelectCommand.Parameters[0].Value = ((string)(search));
+            }
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
